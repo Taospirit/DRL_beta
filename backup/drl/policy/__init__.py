d780494878e80ea1e0c2a520f173ba031832bc96
lintao <lintao209@outlook.com>
2020-08-03 22:21:48 +0800

    remove backup

--- backup/drl/policy/__init__.py
+++
@@ -1,9 +0,0 @@
-from drl.policy.model_free.a2c import A2C
-# from drl.policy.model_free.ddpg import DDPG
-# from drl.policy.model_free.dpg import DPG
-# from drl.policy.model_free.pg import PG
-
-
-__all__ = [
-    'A2C',
-]
