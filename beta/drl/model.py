bf9d72c0a621506d79f8fdfdbd16a53284c17513
lintao <lintao209@outlook.com>
2020-08-03 18:42:29 +0800

    add DDPG AC_net

---
+++ beta/drl/model.py
@@ -0,0 +1,74 @@
+import gym, os, time
+import numpy as np
+import matplotlib.pyplot as plt
+from itertools import count
+from collections import namedtuple
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Categorical
+
+class ActorNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.action_head = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        action_score = self.action_head(x)
+        dist = F.softmax(action_score, dim=-1)
+        return dist
+
+class CriticNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.value_head = nn.Linear(hidden_dim, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        state_value = self.value_head(x)
+        return state_value
+
+class ActorCriticNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+
+        self.action_head = nn.Linear(hidden_dim, output_dim)
+        self.value_head = nn.Linear(hidden_dim, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        action_score = self.action_head(x)
+        state_value = self.value_head(x)
+        dist = F.softmax(action_score, dim=-1)
+        return dist, state_value
+
+# DDPG
+class ActorDPG(nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super().__init__()
+        self.net = nn.Sequential(nn.Linear(state_dim, hidden_dim), nn.ReLU(),
+                                 nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                 nn.Linear(hidden_dim, action_dim), nn.Tanh(), )
+    
+    def forward(self, state, noise_std=0.0):
+        action = self.net(state)
+        return action
+
+class Critic(nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super().__init__()
+        # inpur_dim = state_dim + action_dim, 
+        self.net = nn.Sequential(nn.Linear(state_dim + action_dim , hidden_dim), nn.ReLU(),
+                                 nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                 nn.Linear(hidden_dim, 1), )
+
+    def forward(self, state, action):
+        input = torch.cat((state, action), dim=1)
+        q_value = self.net(input)
+        return q_value
