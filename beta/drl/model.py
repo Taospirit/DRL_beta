c457f614b9513195f3dfc57755e31ae75273fc7f
lintao <lintao209@outlook.com>
2020-08-05 23:46:53 +0800

    add ActorPPO

--- beta/drl/model.py
+++ beta/drl/model.py
@@ -10,29 +10,6 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.distributions import Categorical
 
-class ActorNet(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super().__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.action_head = nn.Linear(hidden_dim, output_dim)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        action_score = self.action_head(x)
-        dist = F.softmax(action_score, dim=-1)
-        return dist
-
-class CriticV(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super().__init__()
-        self.fc1 = nn.Linear(input_dim, hidden_dim)
-        self.value_head = nn.Linear(hidden_dim, 1)
-
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        state_value = self.value_head(x)
-        return state_value
-
 class ActorCriticNet(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super().__init__()
@@ -48,6 +25,18 @@ class ActorCriticNet(nn.Module):
         dist = F.softmax(action_score, dim=-1)
         return dist, state_value
 
+class ActorNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.action_head = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        action_score = self.action_head(x)
+        dist = F.softmax(action_score, dim=-1)
+        return dist
+
 # DDPG
 class ActorDPG(nn.Module):
     def __init__(self, state_dim, hidden_dim, action_dim):
@@ -59,6 +48,33 @@ class ActorDPG(nn.Module):
     def forward(self, state, noise_std=0.0):
         action = self.net(state)
         return action
+
+class ActorPPO(nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(state_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.mu_head = nn.Linear(hidden_dim, 1)
+        self.sigma_head = nn.Linear(hidden_dim, 1)
+
+    def forward(self, state):
+        x = F.leaky_relu(self.fc1(state))
+        x = F.leaky_relu(self.fc2(x))
+        mu = self.mu_head(x)
+        sigma = self.sigma_head(x)
+
+        return mu, sigma
+
+class CriticV(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.value_head = nn.Linear(hidden_dim, 1)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        state_value = self.value_head(x)
+        return state_value
 
 class CriticQ(nn.Module):
     def __init__(self, state_dim, hidden_dim, action_dim):
