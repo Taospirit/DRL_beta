58803a8c0ca9370fe799249330324dba3686a999
lintao <lintao209@outlook.com>
2020-08-11 20:39:39 +0800

    refactor test code without policy.sample()

--- beta/test_a2c.py
+++ beta/test_a2c.py
@@ -4,6 +4,7 @@ import torch
 
 from drl.model import ActorNet, CriticV
 from drl.policy import A2CPolicy as Policy
+from drl.buffer import ReplayBuffer as Buffer
 
 env_name = 'CartPole-v0'
 env = gym.make(env_name)
@@ -20,12 +21,13 @@ episodes = 300
 max_step = 1000
 actor_learn_freq=1
 target_update_freq=0
-model_save_dir = './save/test_a2c'
+model_save_dir = './save/test_a2c_buffer'
 os.makedirs(model_save_dir, exist_ok=True)
 
 actor = ActorNet(state_space, hidden_dim, action_space)
 critic = CriticV(state_space, hidden_dim, 1)
-policy = Policy(actor, critic, actor_learn_freq=actor_learn_freq, target_update_freq=target_update_freq)
+buffer = Buffer(max_step, replay=False)
+policy = Policy(actor, critic, buffer, actor_learn_freq=actor_learn_freq, target_update_freq=target_update_freq)
 
 
 def plot(steps, y_label, model_save_dir):
@@ -37,8 +39,9 @@ def plot(steps, y_label, model_save_dir):
     ax.set_ylabel('Run Reward')
     ax.plot(steps)
     RunTime = len(steps)
+
     path = model_save_dir + '/RunTime' + str(RunTime) + '.jpg'
-    if len(steps) % 50 == 0:
+    if len(steps) % 100 == 0:
         plt.savefig(path)
     plt.pause(0.0000001)
 
@@ -57,61 +60,66 @@ def save_setting():
     path = dir_path + model_save_dir[1:] + '/' + model_save_dir.split('/')[-1] + '.txt'
     with open(path, 'w+') as f:
         f.write(data)
+    print (f'Save train setting in {path}!')
+
+def sample(env, policy, max_step, test=False):
+    assert env, 'You must set env for sample'
+    rewards = 0
+    state = env.reset()
+
+    for step in range(max_step):
+        action, log_prob = policy.choose_action(state, test)
+        next_state, reward, done, info = env.step(action)
+        env.render()
+        # process env callback
+        if not test:
+            # policy.process(s=state, a=action, s_=next_state, r=reward, d=done)
+            mask = 0 if done else 1
+            policy.process(s=state, r=reward, l=log_prob, m=mask)
+        
+        rewards += reward
+        if done:
+            break
+        state = next_state
+
+    if not test:
+        policy.learn()
+    return rewards, step
 
 run_type = ['train', 'eval', 'retrain']
 run = run_type[0]
 save_file = model_save_dir.split('/')[-1]
-
 def main():
-    if run == 'train':
-        save_setting()
-        live_time = []
-        for i_eps in range(episodes):
-            step = policy.sample(env, max_step)
-            live_time.append(step)
-            plot(live_time, 'Training_A2C_TwoNet_no_Double', model_save_dir)
-            policy.learn()
-
-        policy.save_model(model_save_dir, save_file)
-        env.close()
-
-    elif run == 'eval':
+    test = False
+    if run == 'eval':
+        global episodes
+        episodes = 100
+        test = True
         print ('Loading model...')
-        policy.load_model(model_save_dir, save_file, test=True)
+        policy.load_model(model_save_dir, save_file, test=test)
 
-        for i in range(100):
-            # reward = policy.sample(env, max_step, test=True)
-            # print (f'eval episode:{i+1}, reward_avg:{reward}')
-            state = env.reset()
-            rewards = 0
-            for step in range(max_step):
-                action = policy.choose_action(state)
-                next_state, reward, done, info = env.step(action)
-                rewards += reward
-                env.render()
-                if done:
-                    state = env.reset()
-                    break
-                state = next_state
-            print (f'Eval eps:{i+1}, Rewards:{rewards}, Steps:{step+1}')
-        env.close()
-
+    elif run == 'train': 
+        print ('Saving model setting...')
+        save_setting()
     elif run == 'retrain':
         print ('Loading model...')
         policy.load_model(model_save_dir, save_file)
-        live_time = []
-
-        for i_eps in range(episodes):
-            reward = policy.sample(env, max_step)
-            live_time.append(reward)
-            plot(live_time, 'Training_A2C_TwoNet_no_Double', model_save_dir)
-            policy.learn()
-        policy.save_model(model_save_dir, save_file)
-        env.close()
-
     else:
         print ('Setting your run type!')
+        return 0
 
+    live_time = []
+    for i_eps in range(episodes):
+        rewards, step = sample(env, policy, max_step, test=test)
+        if run == 'eval':
+            print (f'Eval eps:{i_eps+1}, Rewards:{rewards}, Steps:{step+1}')
+            continue
+        live_time.append(rewards)
+        plot(live_time, 'Training_A2C_TwoNet_no_Double', model_save_dir)
+
+        if i_eps % 1000 == 0:
+            policy.save_model(model_save_dir, save_file)
+    env.close()
 
 if __name__ == '__main__':
     main()
--- beta/test_ddpg.py
+++ beta/test_ddpg.py
@@ -21,8 +21,8 @@ hidden_dim = 32
 episodes = 5000
 max_step = 200
 buffer_size = 50000
-actor_learn_freq = 5
-target_update_freq = 10
+actor_learn_freq = 1
+target_update_freq = 0
 batch_size = 1000
 model_save_dir = './save/test_ddpg'
 os.makedirs(model_save_dir, exist_ok=True)
@@ -33,7 +33,7 @@ buffer = Buffer(buffer_size)
 policy = Policy(actor, critic, buffer, actor_learn_freq=actor_learn_freq, target_update_freq=target_update_freq, batch_size=batch_size)
 
 
-def plot(steps, y_label, plot_save_dir):
+def plot(steps, y_label, model_save_dir):
     ax = plt.subplot(111)
     ax.cla()
     ax.grid()
@@ -43,7 +43,7 @@ def plot(steps, y_label, plot_save_dir):
     ax.plot(steps)
     RunTime = len(steps)
 
-    path = plot_save_dir + '/RunTime' + str(RunTime) + '.jpg'
+    path = model_save_dir + '/RunTime' + str(RunTime) + '.jpg'
     if len(steps) % 100 == 0:
         plt.savefig(path)
     plt.pause(0.0000001)
@@ -63,64 +63,65 @@ def save_setting():
     path = dir_path + model_save_dir[1:] + '/' + model_save_dir.split('/')[-1] + '.txt'
     with open(path, 'w+') as f:
         f.write(data)
+    print (f'Save train setting in {path}!')
+
+def sample(env, policy, max_step, test=False):
+    assert env, 'You must set env for sample'
+    reward_avg = 0
+    state = env.reset()
+
+    for step in range(max_step):
+        action = policy.choose_action(state, test)
+        action = action.clip(-1, 1) * env.action_space.high[0]
+        next_state, reward, done, info = env.step(action)
+        env.render()
+        # process env callback
+        if not test:
+            policy.process(s=state, a=action, s_=next_state, r=reward)
+        
+        reward_avg += reward
+        if done:
+            break
+        state = next_state
+
+    if not test:
+        policy.learn()
+    return reward_avg/(step+1), step
 
 run_type = ['train', 'eval', 'retrain']
 run = run_type[1]
 save_file = model_save_dir.split('/')[-1]
-
 def main():
-    if run == 'train':
-        save_setting()
-        live_time = []
-
-        for i_eps in range(episodes):
-            step = policy.sample(env, max_step)
-            live_time.append(step)
-            plot(live_time, 'Training_DDPG_TwoNet_Double', model_save_dir)
-            loss_actor_avg, loss_critic_avg = policy.learn()
-            print (f'Learing Eps:{i_eps}, Actor_Loss:{round(loss_actor_avg, 3)}, Critic_Loss:{round(loss_critic_avg, 3)}')
-
-        policy.save_model(model_save_dir, save_file)
-        env.close()
-
-    elif run == 'eval':
+    test = False
+    if run == 'eval':
+        global episodes
+        episodes = 100
+        test = True
         print ('Loading model...')
-        policy.load_model(model_save_dir, save_file, test=True)
+        policy.load_model(model_save_dir, save_file, test=test)
 
-        for i in range(100):
-            # reward = policy.sample(env, max_step, test=True)
-            # print (f'eval episode:{i+1}, reward_avg:{reward}')
-            state = env.reset()
-            rewards = 0
-            for step in range(max_step):
-                action = policy.choose_action(state).clip(-1, 1)
-                action *= action_max
-                next_state, reward, done, info = env.step(action)
-                rewards += reward
-                env.render()
-                if done:
-                    state = env.reset()
-                    break
-                state = next_state
-            print (f'Eval eps:{i+1}, Avg_reward:{rewards/(step+1)}, Steps:{step+1}')
-        env.close()
-
+    elif run == 'train': 
+        print ('Saving model setting...')
+        save_setting()
     elif run == 'retrain':
         print ('Loading model...')
         policy.load_model(model_save_dir, save_file)
-        live_time = []
-
-        for i_eps in range(episodes):
-            reward = policy.sample(env, max_step)
-            live_time.append(reward)
-            plot(live_time, 'Training_DDPG_TwoNet_Double', model_save_dir)
-            policy.learn()
-        policy.save_model(model_save_dir, save_file)
-        env.close()
-
     else:
         print ('Setting your run type!')
+        return 0
 
+    live_time = []
+    for i_eps in range(episodes):
+        rewards, step = sample(env, policy, max_step, test=test)
+        if run == 'eval':
+            print (f'Eval eps:{i_eps+1}, Rewards:{rewards}, Steps:{step+1}')
+            continue
+        live_time.append(rewards)
+        plot(live_time, 'Training_DDPG_TwoNet_no_Double', model_save_dir)
+
+        if i_eps % 1000 == 0:
+            policy.save_model(model_save_dir, save_file)
+    env.close()
 
 if __name__ == '__main__':
     main()
