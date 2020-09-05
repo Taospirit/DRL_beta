4e38c5ec56ae85c3d8af7e9c682cd7691f4fbe5c
lintao <lintao209@outlook.com>
2020-09-05 16:15:09 +0800

    add DQN & update test

---
+++ beta/test_dqn.py
@@ -0,0 +1,103 @@
+import gym, os, time
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.nn.functional as F
+import numpy as np
+
+from drl.model import ActorNet, CriticV
+from drl.algorithm import DQN
+from drl.utils import ReplayBuffer as Buffer
+
+env_name = 'CartPole-v0'
+env = gym.make(env_name)
+env = env.unwrapped
+env.seed(1)
+torch.manual_seed(1)
+
+#Parameters
+state_space = env.observation_space.shape[0]
+action_space = env.action_space.n
+action_shape = 0 if isinstance(env.action_space.sample(), int) else env.action_space.sample().shape
+
+hidden_dim = 32
+episodes = 1000
+buffer_size = 2000
+batch_size = 100
+target_update_freq=100
+max_step = 1000
+
+model_save_dir = 'save/test_dqn_double'
+model_save_dir = os.path.join(os.path.dirname(__file__), model_save_dir)
+save_file = model_save_dir.split('/')[-1]
+os.makedirs(model_save_dir, exist_ok=True)
+
+critic = CriticV(state_space, hidden_dim, action_space)
+policy = DQN(critic, action_shape=action_shape, buffer_size=buffer_size, batch_size=batch_size, target_update_freq=100)
+
+def sample(env, policy, max_step, test=False):
+    assert env, 'You must set env for sample'
+    rewards = 0
+    state = env.reset()
+
+    for step in range(max_step):
+        action = policy.choose_action(state, test)
+        next_state, reward, done, info = env.step(action)
+        env.render()
+        # process env callback
+        if not test:
+            mask = 0 if done else 1
+            policy.process(s=state, a=action, r=reward, m=mask, s_=next_state)
+        
+        rewards += reward
+        if done:
+            break
+        state = next_state
+
+    if not test:
+        policy.learn()
+    return rewards, step
+
+from test_tool import policy_test
+run_type = ['train', 'eval', 'retrain']
+run = run_type[0]
+plot_name = 'DQN_Double'
+
+def main():
+    test = False
+    if run == 'eval':
+        global episodes
+        episodes = 100
+        test = True
+        print ('Loading model...')
+        policy.load_model(model_save_dir, save_file, load_actor=True)
+
+    elif run == 'train': 
+        print ('Saving model setting...')
+        # save_setting()
+        policy_test.save_setting(env_name, state_space, action_space, episodes, max_step, policy, model_save_dir, save_file)
+    elif run == 'retrain':
+        print ('Loading model...')
+        policy.load_model(model_save_dir, save_file, load_actor=True, load_critic=True)
+    else:
+        print ('Setting your run type!')
+        return 0
+
+    live_time = []
+    for i_eps in range(episodes):
+        rewards, step = sample(env, policy, max_step, test=test)
+        if run == 'eval':
+            print (f'Eval eps:{i_eps+1}, Rewards:{rewards}, Steps:{step+1}')
+            continue
+        live_time.append(rewards)
+        policy_test.plot(live_time, plot_name, model_save_dir)
+
+        if i_eps > 0 and i_eps % 100 == 0:
+            print (f'i_eps is {i_eps}')
+            policy.save_model(model_save_dir, save_file, save_critic=True)
+    env.close()
+
+if __name__ == '__main__':
+    main()
