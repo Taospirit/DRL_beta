dfc967b82ee10e9119d4814dd0b63ab514ed193a
lintao <lintao209@outlook.com>
2020-09-05 16:22:10 +0800

    refactory dir

---
+++ beta/drl/algorithm/__init__.py
@@ -0,0 +1,14 @@
+from drl.algorithm.base import BasePolicy
+from drl.algorithm.dqn import DQN
+from drl.algorithm.a2c import A2C
+from drl.algorithm.ddpg import DDPG
+from drl.algorithm.ppo import PPO
+
+
+__all__ = [
+    'Base'
+    'DQN'
+    'A2C'
+    'DDPG'
+    'PPO'
+]
